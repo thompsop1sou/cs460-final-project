0a728f4e0be24c44a87e00eb247094d23485dd4d
petermthompson <peter.m.thompson@protonmail.com>
2023-12-04 13:18:24 -0800

    Move front end to reference shared database

--- peter_front_end/final_db_config.php
+++ peter_front_end/final_db_config.php
@@ -1,6 +1,6 @@
 <?php
-$hostname = "localhost";
-$username = "thompsop1";
-$password = "HucjkynLn0gn2A3xTL98WaIsLL0oAU3r";
-$schema = "usr_thompsop1_1";
+    $hostname = "localhost";
+    $username = "thompsop1";
+    $password = "HucjkynLn0gn2A3xTL98WaIsLL0oAU3r";
+    $schema = "f23_motionSense";
 ?>
